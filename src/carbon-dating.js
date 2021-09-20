ff64998ad170559ad4311a22408e8789311b65df
Vostavhy <vostavhy@gmail.com>
2021-09-20 20:03:51 +0300

    implement several tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (Object.prototype.toString.call(sampleActivity) != '[object String]') return false;
+  sampleActivity = +sampleActivity;
+  if (!sampleActivity) return false;
+  if ((sampleActivity <= 0) || (sampleActivity > 15)) return false;
+  const result = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  return result;
 }
--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  let obj = {
+    0: 'winter',
+    1: 'winter',
+    2: 'spring',
+    3: 'spring',
+    4: 'spring',
+    5: 'summer',
+    6: 'summer',
+    7: 'summer',
+    8: 'autumn',
+    9: 'autumn',
+    10: 'autumn',
+    11: 'winter'
+  }
+
+  if (!date) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date)){
+    throw new Error('Invalid date!');
+  }
+  if (Object.getOwnPropertyNames(date).length > 0){
+    throw new Error('Invalid date!');
+  }
+  return obj[date.getMonth()];
 }

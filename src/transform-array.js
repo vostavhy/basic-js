3f098ce20df756156aca821fad38150d17d7f5f2
Vostavhy <vostavhy@gmail.com>
2021-09-20 21:30:23 +0300

    implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,41 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--discard-next') {
+      result.push(null);
+      i++;
+      continue;
+    }
+    if (arr[i] == '--double-next') {
+      if(arr[i + 1]) {
+        result.push(arr[i + 1]);
+        result.push(arr[i + 1]);
+        i++;          
+      }
+      continue;
+    }
+    if (arr[i] == '--discard-prev') {
+      if (result.length > 0) {
+        result.pop();          
+      }
+      continue;
+    }
+    if (arr[i] == '--double-prev') {
+      if ((result.length > 0) && (result[result.length - 1] != null)) {
+        result.push(result[result.length - 1]);       
+      }
+      continue;
+    }
+    result.push(arr[i]);    
+  }
+
+  let result2 = [];
+  for (let el of result) {
+      if (el != null) result2.push(el);
+  }
+  return result2;
 }

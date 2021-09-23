466892eec921908085cf9dceca2c6a7994ce4b98
Vostavhy <vostavhy@gmail.com>
2021-09-23 12:39:22 +0300

    implement recursive-depth

--- test/test.js
+++ test/test.js
@@ -1,33 +1,17 @@
-let chainMaker = {
-  chain: [],
-  getLength() {
-    return this.chain.length;
-  },
-  addLink(value = ' ') {
-    this.chain.push(`(${value})`);
-    return this;
-  },
-  removeLink(position) {
-    if (!this.chain[position - 1]) {
-      throw new Error("You can't remove incorrect link!");
-      this.chain = [];
-      return this;
+class DepthCalculator {
+  count = 1;
+  calculateDepth(arr) {
+    for (let el of arr) {
+      if (Array.isArray(el)) {
+        this.count++;
+        this.calculateDepth(arr.flat());
+        break;
+      }
     }
-    if (position === this.chain.length) {
-      this.chain.pop()
-      return this;
-    }
-    this.chain = this.chain.slice(0, position - 1).concat(this.chain.slice(position));
-    return this;
-  },
-  reverseChain() {
-    this.chain.reverse();
-    return this;
-  },
-  finishChain() {
-    return this.chain.join('~~')
+    return this.count;
   }
-};
+}
 
 
-console.log(chainMaker.addLink(1).addLink(2).finishChain());
+const depthCalculator = new DepthCalculator();
+console.log(depthCalculator.calculateDepth([1, 2, 3, 4, [1, 2, [1, 2, [[[]]]]], 5, [1, [[[[[[]]]]]]]]));

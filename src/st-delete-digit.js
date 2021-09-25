54b93ab57b0609ccef2ecf086615db18d5966602
Vostavhy <vostavhy@gmail.com>
2021-09-25 15:00:16 +0300

    implement st-dd

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  n = n.toString();
+  let max = Number(n[0]);
+  for (let i = 0; i < n.length - 1; i++) {
+    let temRes = n.slice(0, i) + n.slice(i + 1);
+    const number = Number(temRes);
+    if (number > max) max = number;
+  }
+  return max;
 }
--- test/ztest.js
+++ test/ztest.js
@@ -1,38 +1,12 @@
-function getCommonCharacterCount(s1, s2) {
-  let obj1 = {};
-  let obj2 = {};
-
-  for (let char of s1) {
-    if (s2.indexOf(char) === -1) continue;
-    if (obj1[char] === undefined) {
-      obj1[char] = 1;
-      continue;
-    }
-    obj1[char] += 1;
+function deleteDigit(n) {
+  let max = -Infinity;
+  n = n.toString();
+  for (let i = 0; i < n.length - 1; i++) {
+    let temRes = n.slice(0, i) + n.slice(i + 1);
+    const number = Number(temRes);
+    if (number > max) max = number;
   }
-
-  for (let char of s2) {
-    if (s1.indexOf(char) === -1) continue;
-    if (obj2[char] === undefined) {
-      obj2[char] = 1;
-      continue;
-    }
-    obj2[char] += 1;
-  }
-
-  let result = 0;
-
-  for (let key in obj1) {
-    let tempResult = obj1[key] - obj2[key];
-    if ((tempResult === 0) || (tempResult < 0)) tempResult = obj1[key];
-    else tempResult = obj2[key];
-    result += tempResult;
-  }
-
-  return result;
+  return max;
 }
 
-let s1 = 'aabcc';
-let s2 = 'adcaa';
-
-console.log(getCommonCharacterCount(s1, s2));
+console.log(deleteDigit(10));
